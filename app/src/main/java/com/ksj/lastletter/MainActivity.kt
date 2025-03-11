4095577c8a66a529e3af149f4f2d584ccfd7caf0
seejune2 <106861640+seejune2@users.noreply.github.com>
2025-03-11 17:04:25 +0900

    google login 구현중

--- app/src/main/java/com/ksj/lastletter/MainActivity.kt
+++ app/src/main/java/com/ksj/lastletter/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.google.firebase.auth.FirebaseAuth
 import com.ksj.lastletter.ui.theme.LastLetterTheme
 
 class MainActivity : ComponentActivity() {
